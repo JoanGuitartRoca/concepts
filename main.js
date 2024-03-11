27a669a9a9cbbf1ff23dda5cf043fdc38be94922
JordiGuitartR <jguitartr@gmail.com>
2024-03-11 17:32:48 +0100

    cards

--- main.js
+++ main.js
@@ -2,24 +2,38 @@
 //const axios = require('axios');
 
 window.onload = function () {
-    let but = document.getElementById('but');
-    const url='https://jsonplaceholder.typicode.com/users/1';
+    let sec = document.getElementById('thumbnails');
+    const url = 'https://jsonplaceholder.typicode.com/photos';
+    cards();
 
-    but.addEventListener('click', async () => {
-        /* const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        const json=await response.json(); */
+    async function cards() {
         const response = await axios.get(url);
 
         console.log(response.data);
 
-        const { title, id, userId, company } = response.data;
+        for (let i = 0; i <= 20; i++) {
+            const { title, thumbnailUrl } = response.data[i];
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.style.width = '18rem';
+            sec.appendChild(card);
 
-        let p = document.createElement("b")
-        let par = document.createElement("block").appendChild(p);
-        document.body.appendChild(par);
-        p.innerHTML = company.name;
-    });
+            let img = document.createElement('img');
+            img.src = thumbnailUrl;
+            img.classList.add("card-img-top");
+            card.appendChild(img);
 
+            let cardb = document.createElement('div');
+            cardb.classList.add("card-body");
+            card.appendChild(cardb);
 
+            let p = document.createElement('p');
+            p.classList.add("card-text");
+            p.innerHTML = title;
+            cardb.appendChild(p);
+        }
+    }
 }
 
+/* const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const json=await response.json(); */
